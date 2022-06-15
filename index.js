a98f7e9c8bbfb7e2359e4d82d3b5b434b278adbe
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-15 10:05:00 +0300

    commit tree a013f63d05eb0df0f3b4dd63ac5006ab9d7f125f

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = 31;
+const sum = batteryBatches.reduce((previousValue, currentValue) => previousValue + currentValue, 
+    totalBatteries);
+console.log(sum);
